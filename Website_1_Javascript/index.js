506d1c785eeae1e27ebc647c898c526db96c4aff
blue-toaster <127655524+blue-toaster@users.noreply.github.com>
2024-03-20 10:03:36 +1300

    Add files via upload

---
+++ Website_1_Javascript/index.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById("mySideNav").style.width="250px";
+    document.getElementById("indexBody").style.overflow="hidden";
+}
+function closeNav(){
+    document.getElementById("mySideNav").style.width="0";
+    document.getElementById("indexBody").style.overflow="scroll";
+}
